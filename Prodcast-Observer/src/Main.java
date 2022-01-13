09d6fecf3e4054ae4a4cbc14c544d84860daf8d2
MirjamWeirer <mirjam.weirer@gmx.at>
2022-01-13 09:41:32 +0100

    observer pattern übung prodcast

--- Prodcast-Observer/src/Main.java
+++ Prodcast-Observer/src/Main.java
@@ -3,12 +3,14 @@ public class Main {
         ProdcastServer prodcastServer = new ProdcastServer();
         Notebook n = new Notebook(prodcastServer);
         prodcastServer.setProdcast("Neue Studie","www.prodcast.at",10.30);
-
+        prodcastServer.notifyObserver();
         System.out.println("------------");
         Smartphone sp = new Smartphone(prodcastServer);
         prodcastServer.setProdcast("Anstieg an Coronazahlen","www.prodcast.at",4.00);
+        prodcastServer.notifyObserver();
         System.out.println("------------");
         Smartwatch sw = new Smartwatch(prodcastServer);
         prodcastServer.setProdcast("T-Tellen","www.prodcast.at",2.05);
+        prodcastServer.notifyObserver();
     }
 }
